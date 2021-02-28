467b75ffee6cd8f46c59501595d6a04b4f28b4f3
MajdZ <majdzayyad@gmail.com>
2021-02-28 17:53:23 +0200

    Update DiagnolPuzzle.py

--- DiagnolPuzzle.py
+++ DiagnolPuzzle.py
@@ -1,3 +1,4 @@
+# generates a canvas where black is 1 and white is 0
 def canva(canvas):
     res = []
     for c in canvas:
@@ -6,28 +7,28 @@ def canva(canvas):
         if c == '#':
             res.append(1)
     return res
-    
+# generates all prossible diagnols based on the canvas size     
 def slash(n):
     res = []
-    for i in range(0, n):
+    for i in range(0, n): # from top right
         ph = []
         for j in range(0, i+1):
             p = i + j*n - j
             ph.append(p)
         res.append(ph)
-    for i1 in range(0, n):
+    for i1 in range(0, n): # from top left
         ph = []
         for j1 in range(0, i1+1):
             p = n + j1 - i1 + j1*n - 1
             ph.append(p)
         res.append(ph)
-    for i2 in range(0, n):
+    for i2 in range(0, n): # from bottom right
         ph = []
         for j2 in range(0, i2+1):
             p = n*(n-1) - j2 + i2 - j2*n
             ph.append(p)
         res.append(ph)
-    for i3 in range(0, n):
+    for i3 in range(0, n): # from bottom left
         ph = []
         for j3 in range(0, i3+1):
             p = n*n - i3 +j3 - j3*n - 1
@@ -35,12 +36,12 @@ def slash(n):
         res.append(ph)
     return res
             
-
-def solve(canvas, n, slash):
+# solves the problem by comparing diagnol sets in each iteration until canvas is all black
+def solve(canvas, n, slash): 
     res = 0
     while 1:
         count = 0
-        scorelist = []
+        scorelist = [] # which diagnols set has biggest score
         
         
         for c in canvas:
@@ -53,16 +54,16 @@ def solve(canvas, n, slash):
         
         for e in slash:
             score = 0
-            for i in e:
-                if canvas[i] == 0:
+            for i in e: # generating scores based on diagnols that generate the most black cells and the least white cells
+                if canvas[i] == 0: 
                     score += 1
                 elif canvas[i] == 1:
                     score -= 1
             scorelist.append(score)
-        cscore = scorelist.index(max(scorelist))
+        cscore = scorelist.index(max(scorelist)) # which diagnol set has the highest score
         #print(slash[cscore])
         
-        for x in slash[cscore]:
+        for x in slash[cscore]: # reversing the color of every cell that has a diagnol applied to it
             if canvas[x] == 0:
                 canvas[x] = 1
             elif canvas[x] == 1:
@@ -76,19 +77,19 @@ t = input()
 t = int(t)
 c = 1
 res = []
-for tc in range(0, t):    
+for tc in range(0, t):  # runs the solution for each test case  
     canvas = []
     line = []
     n = input()
     n = int(n)
     
-    for i in range(0, n):
-        line = list(input())
-        canvas.extend(line)
-    res.append(str(solve(canva(canvas), n, slash(n))))
+    for i in range(0, n): 
+        line = list(input()) # the input in string form
+        canvas.extend(line) # the generated canvas
+    res.append(str(solve(canva(canvas), n, slash(n)))) # finding the least number of opperations necessary for every canvas
     
 for e in res:
-    print("Case #" + str(c) + ": " + e)
+    print("Case #" + str(c) + ": " + e) # outputing results
     c+=1
     
     
